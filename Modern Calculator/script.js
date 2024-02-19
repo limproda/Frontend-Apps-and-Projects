5d4da43cbb621b807205a075ffc3e27bee184fb3
Luigi Michele Improda <limproda@gmail.com>
2024-02-18 23:46:08 -0600

    Working on functionality

--- Modern Calculator/script.js
+++ Modern Calculator/script.js
@@ -1,23 +1,34 @@
 var numbersDictionary = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-    zero: 0
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+    zero: "0", 
+    divide: "/",
+    multiply: "*",
+    subs: "-",
+    plus: "+"
 };
 
-var numbers = [];
+var numbers = "";
 
 $(".button").on("click", function (e) {
     var id = e.target.id;
-    console.log(id);
-    if ($(e.target).hasClass("numbers")){
-        numbers = numbers + id;
+    if (id === "equal"){
+        numbers = eval(numbers);
+        console.log(numbers);
+    } else if ( id === "ac") {
+        numbers = " ";
+    }else if ( id === "de") {
+        numbers = numbers.slice(0, -1); 
+    }
+    else {
+        numbers = numbers + numbersDictionary[id];
         console.log(numbers);
     }
 
